f10505756d74ad5cece8d8087dcaf1c417187325
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-10-06 15:14:54 -0400

    Import/install marshmallow, add database to app.py

--- server/app.py
+++ server/app.py
@@ -3,15 +3,26 @@
 # Standard library imports
 from flask import Flask
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 
 # Remote library imports
 from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, Api
 
 # Local imports
 from config import app, db, api
 # Add your model imports
 from models import User, Creature, BugBite, BiteTreatment
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+ma = Marshmallow(app)
+api = Api(app)
 
 
 # Views go here!
