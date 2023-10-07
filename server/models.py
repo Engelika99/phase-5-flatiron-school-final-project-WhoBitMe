32891a5604d397e61c3a6e3ea38bc3c5c200a261
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-10-07 08:44:37 -0400

    Create schema structure in models

--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy import CheckConstraint
 from sqlalchemy.orm import validates
+from marshmallow import Schema, fields, validate
 
 db = SQLAlchemy()
 
@@ -69,3 +70,29 @@ class BugBite(db.Model):
     #one to many with BiteTreatment model
     treatment_plan = db.relationship("BiteTreatment", back_populates="bug_bites")
 
+class UserSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    username = fields.String(required=True, validate=validate.Length(min=1))
+    email = fields.String(required=True)
+    password = fields.String(required=True, validate=validate.Length(min=8))
+
+class CreatureSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    bug_name = fields.String(required=True)
+    image = fields.String()
+    bug_description = fields.String(required=True)
+
+
+class BiteTreatmentSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    treatment_plan = fields.String()
+
+
+
+class BugBiteSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    bite_description = fields.String(required=True, validate=validate.Length(min=10))
+    symptoms = fields.String()
+    severity_of_bite = fields.String()
+    treatment_plan_id = fields.Integer(required=True)
+    creatures = fields.list(fields.String())
